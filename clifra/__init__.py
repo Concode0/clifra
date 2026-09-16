85d050e77d51f894ec076ed3fe73623fe3580041
Concode0 <nemonanconcode@gmail.com>
2026-09-16 10:47:19 +0900

    chore: bump version 2.0.3

--- clifra/__init__.py
+++ clifra/__init__.py
@@ -8,7 +8,7 @@ contracts describe coefficient storage; planned operations execute those
 contracts independently of the private planner and kernel implementations.
 """
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 from clifra.core.algebra import AlgebraContext
 from clifra.core.config import AlgebraConfig, make_algebra, make_algebra_from_config
